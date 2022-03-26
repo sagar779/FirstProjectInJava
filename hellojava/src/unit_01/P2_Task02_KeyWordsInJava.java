d65bd847a6d90593027a6f70a740bff2f55a075e
user <user@DESKTOP-MRPFVGK>
2022-03-26 10:23:16 +0530

    2nd commit

---
+++ hellojava/src/unit_01/P2_Task02_KeyWordsInJava.java
@@ -0,0 +1,68 @@
+package unit_01;
+
+public class P2_Task02_KeyWordsInJava {
+	
+		public static void main(string[] args) {
+			
+			System.out.println("jhjhj");
+			
+			int  double =12;
+			
+			int new() {
+				
+			System.out.println();
+			return 0;
+			}
+			
+			int a=10;
+			a=11;
+			
+			
+			ABC int = new ABC();
+					
+
+}
+}
+
+class int {
+	void display() {
+		System.out.println("Another class!");
+	}
+}
+
+class ABC{
+	void display() {
+		System.out.println("Another class!");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
